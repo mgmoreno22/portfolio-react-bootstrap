2f12638a3dd64f8fe69e739d81872da82150b32a
Michael Moreno <mmoreno2222@gmail.com>
2021-02-26 15:00:46 -0800

    navbar links added

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import "../styles/Topbar.css";
 
 export default function Topbar() {
   return (
     <>
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="ml-auto">
+            <Link className="nav-items" to="/">
+              Home
+            </Link>
+            <Link className="nav-items" to="/About">
+              About
+            </Link>
+            <Link className="nav-items" to="/Portfolio">
+              Portfolio
+            </Link>
+            <Link className="nav-items" to="/Contact">
+              Contact
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
