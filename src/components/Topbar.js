3f4109a3bd466e837abd6f1a427fa86080c4b8c6
Michael Moreno <mmoreno2222@gmail.com>
2021-03-01 12:41:06 -0800

    funcitoning navbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import "../styles/Topbar.css";
 
 export default function Topbar() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+      <Navbar variant="light" id="navbar" expand="lg">
+        <Navbar.Brand id="navbar-brand" href="/">
+          Portfolio
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse>
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             <Link className="nav-items" to="/">
               Home
