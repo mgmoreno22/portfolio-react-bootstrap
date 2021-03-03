2810a8b3b2fea43059e67155962eae238f492cb8
Michael Moreno <mmoreno2222@gmail.com>
2021-03-02 18:28:39 -0800

    current projects added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,7 +14,7 @@ export default function Portfolio() {
           </Col>
         </Row>
 
-        {/* START OF CARD ROW */}
+        {/* -----------START OF CARD ROW----------- */}
         <Row>
           {/* START OF A NEW CARD - VIBEE */}
           <Col xs={12} md={6} lg={4}>
@@ -41,7 +41,7 @@ export default function Portfolio() {
                 <Card.Text>
                   <p className="portTech">
                     React, MongoDB, Firebase Auhentication, Socket.io,
-                    Axios/API, Javascript, Bootstrap
+                    Axios/API, Javascript
                   </p>
                 </Card.Text>
                 <Row className="portLinkBody">
@@ -84,7 +84,7 @@ export default function Portfolio() {
                   alt="Buzzkill logo"
                 />
               </a>
-              <Card.Body className="h-100">
+              <Card.Body>
                 <Card.Text>
                   <p className="portAbout text-justify">
                     MySQL/Sequelize-based live database themed around reviewing
@@ -116,6 +116,165 @@ export default function Portfolio() {
                       View
                     </Button>
                   </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          {/* START OF A NEW CARD - UPC */}
+          <Col xs={12} md={6} lg={4}>
+            <Card>
+              <Card.Header>Pub Crawl</Card.Header>
+              <a
+                target="_blank"
+                href="https://willrodgers7.github.io/Ultimate-Pub-Crawl/"
+              >
+                <Card.Img
+                  variant="bottom"
+                  className="portThumb"
+                  src="./assets/images/pubProject1.png"
+                  alt="Ultimate Pub Crawl home page"
+                />
+              </a>
+              <Card.Body>
+                <Card.Text>
+                  <p className="portAbout text-justify">
+                    Javascript-based Pub Crawl Generator where users can
+                    generate and route bars, lounges, and restaurants.
+                  </p>
+                </Card.Text>
+                <Card.Text>
+                  <p className="portTech">
+                    Javascript, HTML/CSS, Axios/API, Materialize Framework,
+                    Geoapify
+                  </p>
+                </Card.Text>
+                <Row className="portLinkBody">
+                  <Col>
+                    <Button
+                      variant="red"
+                      className="portLinks"
+                      target="_blank"
+                      href="https://github.com/WillRodgers7/Ultimate-Pub-Crawl"
+                    >
+                      Github
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button
+                      className="portLinks"
+                      target="_blank"
+                      href="https://willrodgers7.github.io/Ultimate-Pub-Crawl/"
+                    >
+                      View
+                    </Button>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h1 className="headerText">Extra Projects</h1>
+          </Col>
+        </Row>
+
+        {/* -----------START OF CARD ROW----------- */}
+        <Row>
+          {/* START OF A NEW CARD - Daily Planner */}
+          <Col xs={12} md={6} lg={4}>
+            <Card>
+              <Card.Header>Daily Planner</Card.Header>
+              <a
+                target="_blank"
+                href="https://mgmoreno22.github.io/DailyPlanner/"
+              >
+                <Card.Img
+                  variant="bottom"
+                  className="portThumb"
+                  src="./assets/images/DailyPlanner1.png"
+                  alt="Daily Planner website"
+                />
+              </a>
+              <Card.Body>
+                <Card.Text>
+                  <p className="portAbout text-justify">
+                    Program that tracks daily plans using local storage.
+                  </p>
+                </Card.Text>
+                <Card.Text>
+                  <p className="portTech">
+                    Javascript, HTML/CSS, Bootstrap, Moment.js
+                  </p>
+                </Card.Text>
+                <Row className="portLinkBody">
+                  <Col>
+                    <Button
+                      variant="red"
+                      className="portLinks"
+                      target="_blank"
+                      href="https://github.com/mgmoreno22/DailyPlanner"
+                    >
+                      Github
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button
+                      className="portLinks"
+                      target="_blank"
+                      href="https://mgmoreno22.github.io/DailyPlanner/"
+                    >
+                      View
+                    </Button>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          {/* START OF A NEW CARD - ReadMe Generator */}
+          <Col xs={12} md={6} lg={4}>
+            <Card>
+              <Card.Header>ReadMe Generator</Card.Header>
+              <a
+                target="_blank"
+                href="https://mgmoreno22.github.io/DailyPlanner/"
+              >
+                <Card.Img
+                  variant="bottom"
+                  className="portThumb"
+                  src="./assets/images/ReadMeGenerator.png"
+                  alt="Read Me Generator Example Page"
+                />
+              </a>
+              <Card.Body>
+                <Card.Text>
+                  <p className="portAbout text-justify">
+                    Node-based program to generate professional README
+                    templates.
+                  </p>
+                </Card.Text>
+                <Card.Text>
+                  <p className="portTech">Javascript, Node.js</p>
+                </Card.Text>
+                <Row className="portLinkBody">
+                  <Col>
+                    <Button
+                      variant="red"
+                      className="portLinks"
+                      target="_blank"
+                      href="https://github.com/mgmoreno22/MarkdownGenerator"
+                    >
+                      Github
+                    </Button>
+                  </Col>
+                </Row>
+                <Row>
+                  <p className="portTech portClarify text-center">
+                    *This program runs in Terminal
+                  </p>
                 </Row>
               </Card.Body>
             </Card>
