15825c05550233beaf1586894b6874365f89ad69
Michael Moreno <mmoreno2222@gmail.com>
2021-03-02 17:03:31 -0800

    second project added changed card template

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,10 @@ export default function Portfolio() {
             <h1 className="headerText">Main Projects</h1>
           </Col>
         </Row>
+
+        {/* START OF CARD ROW */}
         <Row>
+          {/* START OF A NEW CARD - VIBEE */}
           <Col xs={12} md={6} lg={4}>
             <Card>
               <Card.Header>Vibee</Card.Header>
@@ -25,12 +28,21 @@ export default function Portfolio() {
                   variant="bottom"
                   className="portThumb"
                   src="./assets/images/Vibee.png"
+                  alt="Vibee Home Page"
                 />
               </a>
               <Card.Body>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  <p className="portAbout text-justify">
+                    React-based Music-themed Social Media Website. Users type a
+                    post, choose a song, and post it to a global feed.
+                  </p>
+                </Card.Text>
+                <Card.Text>
+                  <p className="portTech">
+                    React, MongoDB, Firebase Auhentication, Socket.io,
+                    Axios/API, Javascript, Bootstrap
+                  </p>
                 </Card.Text>
                 <Row className="portLinkBody">
                   <Col>
@@ -49,7 +61,59 @@ export default function Portfolio() {
                       target="_blank"
                       href="https://evening-sands-41595.herokuapp.com/"
                     >
-                      Website
+                      View
+                    </Button>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          {/* START OF A NEW CARD - BUZZKILL */}
+          <Col xs={12} md={6} lg={4}>
+            <Card>
+              <Card.Header>Buzzkill</Card.Header>
+              <a
+                target="_blank"
+                href="https://salty-hollows-77148.herokuapp.com/"
+              >
+                <Card.Img
+                  variant="bottom"
+                  className="portThumb"
+                  src="./assets/images/Buzzkill1.png"
+                  alt="Buzzkill logo"
+                />
+              </a>
+              <Card.Body className="h-100">
+                <Card.Text>
+                  <p className="portAbout text-justify">
+                    MySQL/Sequelize-based live database themed around reviewing
+                    and rating public locations.
+                  </p>
+                </Card.Text>
+                <Card.Text>
+                  <p className="portTech">
+                    SQL/Sequelize, Bootstrap, Axios/API, Javascript
+                  </p>
+                </Card.Text>
+                <Row className="portLinkBody">
+                  <Col>
+                    <Button
+                      variant="red"
+                      className="portLinks"
+                      target="_blank"
+                      href="https://github.com/mgmoreno22/BuzzKill"
+                    >
+                      Github
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button
+                      className="portLinks"
+                      target="_blank"
+                      href="https://salty-hollows-77148.herokuapp.com/"
+                    >
+                      View
                     </Button>
                   </Col>
                 </Row>
